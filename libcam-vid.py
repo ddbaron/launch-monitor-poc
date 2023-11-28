06f4a1387ef2d0180a65cb4989680d7088e94667
ddbaron <dbaron.ks@gmail.com>
2023-11-28 14:07:57 -0600

    added

---
+++ libcam-vid.py
@@ -0,0 +1,7 @@
+# library for using libcamera-vid via picamera2
+#
+# video_config = picam2.create_video_configuration(main={"size": (1920, 1080), 'format': 'YUV420'},
+#                                                 controls={'FrameRate': 50, 'NoiseReductionMode': 1})
+# https://github.com/raspberrypi/picamera2/issues/737#issuecomment-1630382707
+
+
