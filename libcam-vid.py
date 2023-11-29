ad8d76af3c2fd4ea3e467400db75b1e0c60db710
ddbaron <dbaron.ks@gmail.com>
2023-11-29 15:56:01 -0600

    clean up

--- libcam-vid.py
+++ libcam-vid.py
@@ -5,3 +5,12 @@
 # https://github.com/raspberrypi/picamera2/issues/737#issuecomment-1630382707
 
 
+from picamera2 import Picamera2, Preview
+import time
+picam2 = Picamera2()
+camera_config = picam2.create_preview_configuration()
+picam2.configure(camera_config)
+picam2.start_preview(Preview.QTGL)
+picam2.start()
+time.sleep(2)
+picam2.capture_file("test.jpg")
