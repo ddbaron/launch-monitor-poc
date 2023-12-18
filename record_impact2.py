b879ecc964a4641a3c78b0e42a9030b0a9a19137
ddbaron <dbaron.ks@gmail.com>
2023-12-18 10:17:26 -0600

    using /dev/shm to reduce frameskips

--- record_impact2.py
+++ record_impact2.py
@@ -8,31 +8,30 @@ import concurrent.futures
 
 class VideoRecorder:
     def __init__(self):
-        self.video_filename = "output.h264"
+        self.video_filename = "/dev/shm/output.h264"
+        self.save_pts = "/dev/shm/tst.pts"
         self.sound_ts = None
         ### 1440x480@132, 1440x320@193, 1200x208@283, 1152x192x304, 672x128@427, 816x144@387
-        self.width = 1440
-        self.height = 320
-        self.fps = 193
-        self.shutter = 250
+        self.width = 1200
+        self.height = 208
+        self.fps = 283
+        self.shutter = 300
+
 
     def media_ctl(self):
          # Loop through media devices and set V4L2 format
         offset_x = (1456 - self.width) // 2
         offset_y = (1088 - self.height) // 2
 
+        print(f"calling media-ctl")
+
         for m in range(0, 6):
             try:
-                # gscrop: (144, 448)/1152x192 crop
-                # mine: (576, 96)/880x192 crop
-                
                 command = f'media-ctl -d /dev/media{m} --set-v4l2 "\'imx296 10-001a\':0 [fmt:SBGGR10_1X10/{self.width}x{self.height} crop:({offset_x},{offset_y})/{self.width}x{self.height}]"'
                 print(f"Command: {command}")
                 subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 
-                #print(f"Success configuring media-ctl on /dev/media{m}\n")
                 return f'/dev/media{m}'
-                break
             except subprocess.CalledProcessError as e:
                 print(f"Error configuring media-ctl: {e}")
         return None
@@ -41,7 +40,8 @@ class VideoRecorder:
         try:
             # Record video using libcamera-vid with circular buffer
             # --width 1152 --height 192 --denoise cdn_off --framerate 304 --shutter 1000 -o {self.video_filename} -n
-            record_cmd = f"libcamera-vid --circular 1 --inline --width {self.width} --height {self.height} --framerate {self.fps} --shutter {self.shutter} --denoise cdn_off  -t 0 -o {self.video_filename} -n"
+            record_cmd = f"libcamera-vid --circular 1 --inline --width {self.width} --height {self.height} --framerate {self.fps} --shutter {self.shutter} --denoise cdn_off --save-pts {self.save_pts} -t 0 -o {self.video_filename} -n"
+            print(f"libcamera-vid config: {record_cmd}.")
             subprocess.run(record_cmd, shell=True, check=True)
         except subprocess.CalledProcessError as e:
             print(f"Error during video recording: {e}")
@@ -62,12 +62,26 @@ class VideoRecorder:
 
     def convert_to_mp4(self):
             # Convert the cropped video to MP4 using ffmpeg
-            mp4_cmd = f"ffmpeg -y -r {int(self.fps) / 10} -i {self.video_filename} {self.video_filename}.mp4"
+            mp4_cmd = f"ffmpeg -y -r {int(self.fps) / 10} -i {self.video_filename} impact.mp4"
             subprocess.run(mp4_cmd, shell=True, check=True)
+
+    def ptsanalyze(self):
+        try:
+            # Remove existing timestamp analysis file
+            tstamps_cmd = f"rm -f tstamps.csv"
+            subprocess.run(tstamps_cmd, shell=True, check=True)
+
+            # Run the ptsanalyze bash script with the specified input file
+            pts_cmd = f"ptsanalyze {self.save_pts}"
+            subprocess.run(pts_cmd, shell=True, check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error during ptsanalyze: {e}")
 
     def run(self):
 
-        if(self.media_ctl):
+        # Configure media-ctrl
+        successful_device = self.media_ctl()
+        if successful_device:
             print(f"Successful configuration on device: {successful_device}")
         else:
             print("No successful configuration")
@@ -107,8 +121,10 @@ class VideoRecorder:
 
         # Continue with other processing after both tasks are complete
         print("Video recording and sound detection completed. Converting to mp4 video.")
-
         self.convert_to_mp4()
+
+        # Run ptsanalyze to determine fps and frameskips
+        self.ptsanalyze()
 
 if __name__ == "__main__":
     video_recorder = VideoRecorder()
